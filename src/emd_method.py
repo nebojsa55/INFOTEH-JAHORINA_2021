0e6edf13b3e93b3563b37115dbf1bf9a778d5f65
Nebojsa Jovanovic <nebojsa.php@gmail.com>
2021-02-27 00:56:17 +0100

    moved to src

---
+++ src/emd_method.py
@@ -0,0 +1,100 @@
+"""
+@authors: Nebojsa Jovanovic (nebojsa.php@gmail.com)
+          Nenad B. Popovic (nenad.pop92@gmail.com)
+          Nadica Miljkovic (nadica.miljkovic@etf.rs)
+          Faculty of Electrical Engineering, University of Belgrade
+         
+"""
+
+import os
+import numpy as np
+import scipy.signal
+import emd
+from utils import direct_autocorr
+from utils import FFT_max
+from noise_modeling import *
+import matplotlib.pyplot as plt
+import tqdm
+import time
+
+if __name__ == '__main__':
+
+    # Parameters
+    N = 2400 
+    Nfft = 4096
+    fs = 2 # Hz
+    path = os.path.abspath(os.getcwd()) + '\Database'
+    LF = 0.016 # Hz
+    HF = 0.25 # Hz
+    pmax_ref = 0.5
+        
+    # Load EGG signal
+    data = np.loadtxt(path + '\ID1_fasting.txt').transpose()
+    egg = data[1,:]
+    
+    # Preprocessing
+    b,a = scipy.signal.butter(3,(LF,HF),'bandpass')
+    egg = scipy.signal.filtfilt(b,a,egg)
+    
+    # Dominant frequency
+    fftegg,df = FFT_max(egg,Nfft)
+    
+    noise_range = np.arange(-50,20,5)
+    RD = []
+    cert = []
+    TIMES = []
+
+    for snr in tqdm.tqdm(noise_range, total = len(noise_range)):
+        # Adding noise 
+        np.random.seed(0)
+        # egg_noise = AWGN(egg,snr)
+        egg_noise = AWGN_dynamic(egg,snr)
+        
+        # Performing EMD
+        start = time.process_time()
+        # imfs = emd.sift.sift(egg_noise)
+        imfs = emd.sift.ensemble_sift(egg_noise,nensembles = 64,max_imfs = 5)
+        TIMES.append(time.process_time()-start)
+        Nimfs = len(imfs[0,:])
+        # emd.plotting.plot_imfs(imfs, cmap=True, scale_y=True)
+        
+        # Statistical parameters from IMFs
+        accPeaks = [direct_autocorr(imfs[:,i],0,2400,30,67)[1] for i in range(Nimfs)]
+        dfs = [FFT_max(imfs[:,i],Nfft)[-1] for i in range(Nimfs)]
+                       
+        #%% First part - elimination based on FFT
+        
+        sig1 = imfs[:,(np.array(dfs)>0.0167) & (np.array(dfs)<0.167)]
+        Nsig = len(sig1[0,:])
+        
+        #%% Second part - autocorrelation
+    
+        pmax = 0
+        for numOfMixed in range(Nsig):
+            for i in range(Nsig-numOfMixed):
+                temp = sig1[:,i]
+                for j in range(numOfMixed):
+                    temp = temp + sig1[:,i+j+1]
+                _,peak,_ = direct_autocorr(temp,0,2400,30,67)
+                if peak>pmax:
+                    pmax = peak
+                    sig2 = temp
+        #%%
+        fft2,df2 = FFT_max(sig2,Nfft)
+        cc = pmax/pmax_ref*100
+        
+        if cc>100: cc = 100
+        
+        RD.append(np.abs(df2-df)*100/df)
+        cert.append(cc)
+    #%%
+    
+    plt.figure()
+    plt.stem(noise_range,RD)
+    plt.xlabel('SNR [dB]')
+    plt.ylabel('RD[%]')
+    plt.figure()
+    plt.stem(noise_range,cert)
+    plt.xlabel('SNR [dB]')
+    plt.ylabel('Certainty [%]')
+    
---
+++ src/utils.py
@@ -0,0 +1,118 @@
+"""
+@authors: Nebojsa Jovanovic (nebojsa.php@gmail.com)
+          Nenad B. Popovic (nenad.pop92@gmail.com)
+          Nadica Miljkovic (nadica.miljkovic@etf.rs)
+          Faculty of Electrical Engineering, University of Belgrade
+         
+"""
+import numpy as np
+import scipy as sp
+import scipy.signal
+import scipy.fftpack
+import pandas as pd
+import math
+
+def direct_autocorr(signal,start,samples,k_begin,k_end):
+    
+    """     
+    Calculates the normalized autocorrelation of the signal in the range
+    from "k_begin" to "k_end" in the signal "signal".  
+    
+    Inputs:
+        signal -> signal for which normalized autocorrelation is calculated
+        start -> index of the starting position of the signal for which 
+                 autocorrelation is calculated
+        samples -> the number of signal samples for which autocorrelation is 
+                   calculated
+        k_begin -> the initial lag of autocorrelation
+        k_end -> the final lag of autocorrelation
+        
+    Outputs:
+        acc_direct -> the normalized autocorrelation signal
+        peak_direct -> value of the norm. acc. peak
+        index_direct -> index of the norm. acc. peak
+    
+    """
+    
+    x = signal[start:start+samples]
+    N = np.size(x)
+    
+    # Compute the normalized autocorrelation
+    acc_direct = []
+    
+    for k in range(k_begin,k_end):
+        numerator = np.sum(x[0:N-k-1]*x[k:N-1])
+        denominator = np.sqrt(np.sum(x[k:N-1]*x[k:N-1]))*np.sqrt(np.sum(x[0:N-k-1]*x[0:N-k-1]))
+        acc_direct.append(numerator/denominator)
+        
+    index,properties = sp.signal.find_peaks(acc_direct, height = (0.001,1))
+    
+    if index.any():
+        index_direct = index[0] + k_begin
+        peak_direct = properties['peak_heights']
+        peak_direct = peak_direct[0]
+    else:
+        peak_direct = 0
+        index_direct = 0
+    
+    return acc_direct,peak_direct,index_direct
+
+
+def PSD_welch(signal,fs,window):
+    
+    """
+
+    Calculates the Welch's spectrum given the scale which
+    determines the lenght of the window like N/scale where 
+    N is the signal length
+
+    Inputs:
+        signal -> signal for which Welch's spectrum is calculated
+        fs -> sample frequency
+        scale -> window_length = len(signal)/scale
+        
+    Outputs:
+        PSD -> power spectral density
+        f_PSD -> frequency axis of the calculated PSD
+        f_dom -> the value of the dominant frequency in the given PSD   
+    """
+    
+    noverlap = (240,window/2)[window <= 480]
+    f_PSD,PSD = sp.signal.welch(signal,fs,nperseg = window,noverlap = noverlap)
+
+    f_dom = np.argmax(PSD[0:int(np.round(np.size(PSD)*0.08))])
+    f_dom = f_PSD[f_dom]
+    
+    return PSD,f_PSD,f_dom
+
+
+def FFT_max(signal,Nfft):
+    
+    """
+    
+    Find the maximum in FFT of the signal
+    
+    Inputs:
+        signal -> signal for which FFT is calculated
+        Nfft -> the number of points at which the FFT is calculated
+        
+    Outputs:
+        fftsig -> Fast Fourier Transform of the given signal
+        f_dom -> the value of the dominant frequency in the given FFT
+    """
+    
+    fftsig = sp.fftpack.fft(signal,Nfft)
+    fftsig = fftsig[0:Nfft//2]
+    fftsig = np.abs(fftsig)
+    
+    border_frequency = 0.167
+    index = int(np.round(Nfft/2*border_frequency))
+    f_dom = np.argmax(fftsig[0:index])
+    f_dom = f_dom/(Nfft/2)
+
+    return fftsig,f_dom
+    
+
+  
+
+    
